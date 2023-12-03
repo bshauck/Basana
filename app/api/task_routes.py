6c692595a7f956e0e5ecf6178946cb49a775e301
bshauck <williamrshauck@gmail.com>
2023-12-03 13:14:38 -0700

    skeleton for task; main landing init; load a few users

---
+++ app/api/task_routes.py
@@ -0,0 +1,92 @@
+from flask import Blueprint, request
+from flask_login import login_required, current_user
+from app.models import Task, db
+from app.forms import TaskForm, error_message, error_messages
+
+task_routes = Blueprint('tasks', __name__)
+
+@task_routes.route('')
+@login_required
+def get_all_tasks():
+    """
+    Query for all Tasks and returns them in a list of dictionaries
+    """
+    print("DB: about to get all tasks")
+    tasks = Task.query.all()
+    print("DB: tasks", tasks)
+    return {"tasks": [task.to_dict() for task in tasks]}
+
+
+@task_routes.route('/<int:id>')
+@login_required
+def get_task(id):
+    """
+    Query for a task by id and returns that task in a dictionary
+    """
+    task = Task.query.get(id)
+    return task.to_dict()
+
+
+@task_routes.route('/new', methods=["POST"])
+@login_required
+def create_task():
+    """
+    Creates a new task and returns the new task in a dictionary
+    """
+
+    form = TaskForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        new_task = {
+            "owner": current_user.id,
+            "name": form.name.data,
+        }
+
+        task = task(**new_task)
+        db.session.add(task)
+        db.session.commit()
+        return task.to_dict(), 201
+    elif form.errors:
+        return error_messages(form.errors), 401
+    else:
+        return error_message("unknown", "An unknown Error has occurred"), 500
+
+@task_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_task(id):
+    """
+    Updates a task and rturns the updated task in a ictionary
+    """
+
+    task = Task.query.get(id)
+
+    form = TaskForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        task.name = form.name.data
+        db.session.add(task)
+        db.session.commit()
+        return task.to_dit(), 201
+    elif form.errors:
+        return error_messages(form.errors), 401
+    else:
+        return error_message("unknown", "An unknown Error has occurred"), 500
+
+@task_routes.route('/<int:id>', methods=["DELETE"])
+@login_required
+def delete_task(id):
+    """
+    Deletes an task and returns a message if successfully deleted
+    """
+    task = Task.query.get(id)
+
+    if task.owner != current_user.id:
+        return error_message("user", "Authorization Error"), 403
+
+    db.session.delete(task)
+    db.session.commit()
+    return {"message": "task successfully deleted"}
--- react-app/src/store/csrf.js
+++ react-app/src/store/csrf.js
@@ -7,11 +7,16 @@ export async function csrfFetch(url, options = {}) {
   const bodyClasses = document.body.classList;
   bodyClasses.add("waiting");
 
-  // set options.method to 'GET' if there is no method
-  options.method = options.method || 'GET';
-  // If multi-part leave it alone, browsers will thingsa utomatically
-  if (!(options.headers && options.headers['Content-Type'] === "multipart/form-data")) {
-    // set options.headers to an empty object if there are no headers
+  // If body is FormData leave it alone, browsers will handle it automatically
+  console.log("csrf TYPEOF body", typeof options?.body)
+
+  if (!(options?.body instanceof FormData)) {
+    console.log("csrfFetch: not form data");
+
+    // set options.method to 'GET' if no method
+    options.method = options.method || 'GET';
+
+    // init options.headers if no headers
     options.headers = options.headers || {};
 
     // if the options.method is not 'GET', then set the "Content-Type" header to
@@ -24,31 +29,33 @@ export async function csrfFetch(url, options = {}) {
         options.headers['Content-Type'] = "application/json";
       // options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
     }
-    // call the default window's fetch with the url and the options passed in
-    let res;
-      try {
-        console.log("fetching", url, options);
-        console.trace();
-        res = await window.fetch(url, options);
-      } catch (error) {
-        // console.error(error);
-        error.status = error.status || 500;
-        if (error.errors) error.errors.fetch = "Failed to Fetch"
-        else error.errors = {"fetch": "Failed to Fetch"}
-        res = error;
-      }
+  } else console.log("csrfFetch: is form data");
+  // call the default fetch with the url and the options passed in
+  let res;
+  try {
+    console.log("fetching", url, options);
+    // console.trace();
+    res = await fetch(url, options);
+  } catch (error) {
+    console.error(error);
+    error.status = error.status || 500;
+    if (error.errors) error.errors.fetch = "Failed to Fetch"
+    else error.errors = {"fetch": "Failed to Fetch"}
+    res = error;
+  }
+  finally {
+    bodyClasses.remove("waiting");
+  }
 
     // if the response status code is 400 or above, then throw an error with the
       // error being the response
 
-    bodyClasses.remove("waiting");
     // if (res.status >= 400) throw res;
 
     // if the response status code is under 400, then return the response to the
       // next promise chain
     return res;
   }
-}
 
 export const fetchData = (url, options) => {
   /* Returns Promise which resolves to either data or errors */
