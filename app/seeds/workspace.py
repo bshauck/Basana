9d04217d18c927f319e8429ead03f02d8b71c7af
bshauck <williamrshauck@gmail.com>
2023-12-21 19:28:04 -0700

    additional seeding; fixed task dnd; fixed setting selected project

--- app/seeds/workspace.py
+++ app/seeds/workspace.py
@@ -6,19 +6,20 @@ import random
 def seed_workspaces():
     users = getUsers()
     for user in users:
-        if random.choice([True, False]):  # whether to create a new team
+        if user.email == 'demo@aa.io' or random.choice([True, False]):  # whether to create a new team
             team_size = random.randint(2, 5)
             team_members = random.sample(users, team_size)
             if user not in team_members:
                 team_members.append(user)
+            times = 5 if user.email == 'demo@aa.io' else 1
 
             # Create new Workspace
-            new_workspace = Workspace(name=nextTeamName(), owner=user)
-
-            # Add users to the Workspace
-            new_workspace.teammates.extend(team_members)
-            db.session.add(new_workspace)
-            db.session.commit()
+            for _ in range(times):
+                new_workspace = Workspace(name=nextTeamName(), owner=user)
+                # Add users to the Workspace
+                new_workspace.teammates.extend(team_members)
+                db.session.add(new_workspace)
+                db.session.commit()
 
 
 def undo_workspaces():
